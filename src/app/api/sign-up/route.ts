b5283d3e5ffd1327bcc65e1dff1e24af4bb39162
Rushikesh Sapkal <rushikeshsapkal930@gmail.com>
2025-09-12 20:24:07 +0530

    Error fixed

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -1,23 +1,21 @@
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
 import bcrypt from "bcryptjs";
-import { sendVerification } from "@/helpers/sendVerificationEmail";
+import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
 
 export async function POST(request: Request) {
     await dbConnect();
     const { email, username, password } = await request.json();
 
     // Check if user already exists
-    const existingUser = await User .findOne({ email });
+    const existingUser = await UserModel.findOne({ email });
     if (existingUser) {
         return new Response(JSON.stringify({ success: false, message: "User already exists" }), { status: 400 });
     }   
     // Hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Create a new user
-    const user = new User({ email, username, password: hashedPassword });
-    await user.save();
-    // Send verification email
-    const verifyCode = Math.floor(100000 + Math.random() * 900000).toString();      
-    await sendVerification(email, username, verifyCode);
+    
+}
+
+     
